f32fe4cc3b03747bbf84c98d1c559dc9560971c7
DmitriVentro <56494597+DmitriVentro@users.noreply.github.com>
2020-12-03 20:09:01 +0700

    Revert "Update Widget.js"
    This reverts commit 5ba89f0ec7f5f9c706a9d67760fffa2a762bb110.

--- Widget.js
+++ Widget.js
@@ -146,7 +146,6 @@ module.exports.Widget = class Widget {
                     }
                 }
                 this.x += this.X_DISTANCE;
-                this.y = response.response.data.y;
             }
             // this.send(this.data.Practice[res], this.data.Name[res], this.preId)
 
@@ -179,7 +178,6 @@ module.exports.Widget = class Widget {
                     }
                 }
                 this.x += this.X_DISTANCE;
-                this.y = response.response.data.y;
             }
         }
     }
